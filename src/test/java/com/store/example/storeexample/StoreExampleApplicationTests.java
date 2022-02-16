e72c2814f7c4193a389d0b6538f76e5cf6202d5c
Felix Scheidel <felixscheidel@gmail.com>
2022-02-16 21:42:00 +0100

    fix typo and add different application ids for both consumers

--- src/test/java/com/store/example/storeexample/StoreExampleApplicationTests.java
+++ src/test/java/com/store/example/storeexample/StoreExampleApplicationTests.java
@@ -3,8 +3,10 @@ package com.store.example.storeexample;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
 @SpringBootTest
+@EmbeddedKafka
 class StoreExampleApplicationTests {
 
 	@Test
